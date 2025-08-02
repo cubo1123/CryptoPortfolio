2b09d21893a8ddd58f36c876301265dffd2e1546
Enrique Emmanuel Perez Ramirez <cubo.enrique@gmail.com>
2025-08-02 17:18:18 -0600

    navigation implementation

---
+++ app/src/main/java/com/cubo1123/cryptoportfolio/presentation/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.cubo1123.cryptoportfolio.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.cubo1123.cryptoportfolio.presentation.coin_detail.CoinDetailScreen
+import com.cubo1123.cryptoportfolio.presentation.coin_list.CoinListScreen
+import com.cubo1123.cryptoportfolio.presentation.ui.theme.CryptoPortfolioTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CryptoPortfolioTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ){
+                        composable(route = Screen.CoinListScreen.route){
+                            CoinListScreen(navController)
+                        }
+
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ){
+                            CoinDetailScreen()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
