fa5368271d15d7b5bcaf47740828b329c302cbfa
Enrique Emmanuel Perez Ramirez <cubo.enrique@gmail.com>
2025-08-02 13:46:15 -0600

    dto por gson

---
+++ app/src/main/java/com/cubo1123/cryptoportfolio/data/remote/dto/LinksExtended.kt
@@ -0,0 +1,7 @@
+package com.cubo1123.cryptoportfolio.data.remote.dto
+
+data class LinksExtended(
+    val stats: Stats,
+    val type: String,
+    val url: String
+)
---
+++ app/src/main/java/com/cubo1123/cryptoportfolio/data/remote/dto/Whitepaper.kt
@@ -0,0 +1,6 @@
+package com.cubo1123.cryptoportfolio.data.remote.dto
+
+data class Whitepaper(
+    val link: String,
+    val thumbnail: String
+)
